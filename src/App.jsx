141bbd78f3cb8190f5ece1fede83eec3ab291939
GustavoGJesus <ggomesdejesus01@gmail.com>
2022-11-08 23:57:31 -0300

    feat: finish sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './global.css'
+import styles from './App.module.css'
 
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
 
 
 function App() {
   return (
    <>
     <Header />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        posts
+      </main>
+    </div>
    </>
   )
 }
