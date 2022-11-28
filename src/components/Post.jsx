1857cc6ff368def2089d6d3ee667baf4b2277049
GustavoGJesus <ggomesdejesus01@gmail.com>
2022-11-28 02:13:28 -0300

    feat: create form of comments and styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -27,6 +27,17 @@ export function Post() {
         </p>
         <p><a href=""> #reactjs</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
+          <textarea 
+            placeholder='Deixe um comentário'
+          />
+
+          <footer>
+            <button type='submit'>Publicar</button>
+          </footer>
+      </form>
     </article>
   );
 }
