31134b331ece58bb4a703b3bffef42595c5c3b7a
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-01-20 00:14:10 -0300

    feat: create array with posts mock ddata

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,54 @@ import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/GustavoGJesus.png",
+      name: "Gustavo Gomes",
+      role: "Front-End Engineer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galera 👋🏽",
+      },
+      {
+        type: "paragraph",
+        content: "Primeiro módulo de react do ignite da Rocketseat",
+      },
+      {
+        type: "link",
+        content: '👉🏽 rocketseat.com',
+      },
+    ],
+    publishedAt: new Date("2023-01-19 23:40:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/GustavoGJesus.png",
+      name: "Gustavo Gomes",
+      role: "Front-End Engineer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galera 👋🏽",
+      },
+      {
+        type: "paragraph",
+        content: "Primeiro módulo de react do ignite da Rocketseat",
+      },
+      {
+        type: "link",
+        content: '👉🏽 rocketseat.com',
+      },
+    ],
+    publishedAt: new Date("2023-01-19 23:40:00"),
+  },
+];
 
 function App() {
   return (
@@ -14,8 +62,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
